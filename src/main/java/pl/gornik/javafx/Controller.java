c29e1203f9d8b05eb3a2415522ed2eba4aef3da7
MiloszKrakowiakDEV <miloszz.krakowiak@gmail.com>
2025-03-10 22:55:11 +0100

    Goat displays above squares

--- src/main/java/pl/gornik/javafx/Controller.java
+++ src/main/java/pl/gornik/javafx/Controller.java
@@ -2,6 +2,7 @@ package pl.gornik.javafx;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -37,6 +38,7 @@ public class Controller {
         if(GridPane.getRowIndex(player)==0){
             info.setText("Nie można iść w górę!");
         }else{
+            player.toFront();
             GridPane.setRowIndex(player,GridPane.getRowIndex(player)-1);
             grid.requestFocus();
         }
@@ -45,6 +47,7 @@ public class Controller {
         if(GridPane.getRowIndex(player)==grid.getRowCount()-1){
             info.setText("Nie można iść w dół!");
         }else{
+            player.toFront();
             GridPane.setRowIndex(player,GridPane.getRowIndex(player)+1);
         }
     }
@@ -52,6 +55,7 @@ public class Controller {
         if(GridPane.getColumnIndex(player)==grid.getColumnCount()-1){
             info.setText("Nie można iść w prawo!");
         }else{
+            player.toFront();
             GridPane.setColumnIndex(player,GridPane.getColumnIndex(player)+1);
         }
     }
@@ -59,6 +63,7 @@ public class Controller {
         if(GridPane.getColumnIndex(player)==0){
             info.setText("Nie można iść w lewo!");
         }else{
+            player.toFront();
             GridPane.setColumnIndex(player,GridPane.getColumnIndex(player)-1);
         }
     }
@@ -68,26 +73,36 @@ public class Controller {
                 Pane pane = new Pane();
                 Label label = new Label();
                 label.setText(String.valueOf(j+1+grid.getColumnCount()*i));
+                label.setStyle("-fx-font-size:20px;");
+                label.setPadding(new Insets(0,0,0,2));
                 pane.getChildren().add(label);
                 pane.setStyle("-fx-border-color: black;\n" +
                         "    -fx-border-width: 2px;\n" +
-                        "    -fx-padding: 10px;");
+                        "    -fx-padding: 10px;-fx-background-color: red;");
                 grid.add(pane,j,i);
 
             }
         }
+        player.toFront();
+
     }
     public void moveToSpace(){
         String input = userField.getText();
         if(input.matches("^[0-9]+$")){
             int inputInt = Integer.valueOf(input);
-            int col = (inputInt-1)%grid.getColumnCount();
-            int row = (inputInt-1-col)% grid.getRowCount();
-            System.out.println(row);
-            System.out.println(col);
-            GridPane.setColumnIndex(player,col);
-            GridPane.setRowIndex(player,row);
-            info.setText("Poprawnie przeniesiono na pole "+input);
+            if(inputInt <= grid.getColumnCount()* grid.getRowCount()){
+                int col = (inputInt-1)%grid.getColumnCount();
+                int row = (inputInt-1-col)% grid.getRowCount();
+                System.out.println(row);
+                System.out.println(col);
+                GridPane.setColumnIndex(player,col);
+                GridPane.setRowIndex(player,row);
+                info.setText("Poprawnie przeniesiono na pole "+input);
+                player.toFront();
+
+            }else{
+                info.setText("Podana liczba nie mieści się w siatce!");
+            }
         }else{
             info.setText("Nie podano liczby!");
         }
